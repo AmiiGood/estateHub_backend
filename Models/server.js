60f682e23eaa1ee7efc22695e4200f960e357cef
oscar <oscarmancilla227@gmail.com>
2025-10-10 13:43:01 -0600

    add: geocodificador agregado

--- Models/server.js
+++ Models/server.js
@@ -1,53 +1,40 @@
-import express from 'express';
-import cors from 'cors';
-import pool from '../Config/connection.js'
-import start_router from '../Routes/startRoutes.js';
-
+import express from "express";
+import cors from "cors";
+import pool from "../Config/connection.js";
+import start_router from "../Routes/startRoutes.js";
+import geocodificadorRoutes from "../Routes/geocodificadorRoutes.js";
 
 export class Server {
+  constructor() {
+    this.app = express();
+    this.port = 3000;
+    this.connection();
+    this.middlewares();
+    this.routes();
+  }
 
-    constructor(){
+  middlewares() {
+    this.app.use(cors());
+    this.app.use(express.json());
+  }
 
-        this.app = express();
-        this.port = 3000;
-        this.connection();
-        this.middlewares();
-        this.routes();
+  async connection() {
+    try {
+      await pool;
+      console.log("Conectado");
+    } catch (e) {
+      console.log(e);
     }
+  }
 
+  routes() {
+    this.app.use("/api/start", start_router);
+    this.app.use("/geocodificador", geocodificadorRoutes);
+  }
 
-    middlewares() {
-
-        this.app.use(cors());
-        this.app.use(express.json());
-    };
-
-
-    async connection(){
-
-        try {
-
-            await pool;
-            console.log("Conectado")
-
-        }catch(e){
-            console.log(e);
-        }
-        
-    };
-
-    routes(){
-        this.app.use('/api/start', start_router);
-    }
-
-    startServer(){
-
-        this.app.listen(this.port, () => {
-            console.log(`Servidor corriendo en el puerto ${this.port}`)
-        });
-
-    }
-
-
-
-};
+  startServer() {
+    this.app.listen(this.port, () => {
+      console.log(`Servidor corriendo en el puerto ${this.port}`);
+    });
+  }
+}
---
+++ Routes/geocodificadorRoutes.js
@@ -0,0 +1,8 @@
+import { Router } from "express";
+import { buscadorInfo } from "../Controllers/geocodificadorController.js";
+
+const geocodificadorRouter = Router();
+
+geocodificadorRouter.get("getInfo", buscadorInfo);
+
+export default geocodificadorRouter;
