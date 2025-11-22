72812de007ce473ce580baaf9e6d31b019f3efa0
AmiiGood <alexisalvarez1234563@outlook.com>
2025-11-22 08:07:30 -0600

    update: citas

--- Routes/citasRoutes.js
+++ Routes/citasRoutes.js
@@ -11,25 +11,47 @@ import {
   obtenerCitasPorRangoFechas,
   obtenerCitasPorPropiedad,
   actualizarEstatusCita,
+  obtenerHorariosDisponibles,
 } from "../Controllers/citasController.js";
 import { verificarToken } from "../Middlewares/auth.js";
 
 const citasRouter = Router();
 
-citasRouter.get("/getCitas",verificarToken, obtenerCitas);
-citasRouter.get("/getCita/:idCita",verificarToken, obtenerCitaPorId);
-citasRouter.get("/getCitasByUsuario/:idUsuario",verificarToken, obtenerCitasPorUsuario);
+citasRouter.get("/getCitas", verificarToken, obtenerCitas);
+citasRouter.get("/getCita/:idCita", verificarToken, obtenerCitaPorId);
+citasRouter.get(
+  "/getCitasByUsuario/:idUsuario",
+  verificarToken,
+  obtenerCitasPorUsuario
+);
 citasRouter.get(
   "/getCitasByResponsable/:idUsuario",
   verificarToken,
   obtenerCitasPorResponsable
 );
-citasRouter.get("/getCitasByPropiedad/:idPropiedad",verificarToken, obtenerCitasPorPropiedad);
-citasRouter.get("/getCitasByEstatus/:estatus",verificarToken, obtenerCitasPorEstatus);
-citasRouter.get("/getCitasByFechas",verificarToken, obtenerCitasPorRangoFechas);
-citasRouter.post("/postCita",verificarToken, crearCita);
-citasRouter.put("/putCita",verificarToken, actualizarCita);
-citasRouter.delete("/deleteCita/:idCita",verificarToken, eliminarCita);
-citasRouter.patch("/patchCita/:idCita/estatus",verificarToken, actualizarEstatusCita);
+citasRouter.get(
+  "/getCitasByPropiedad/:idPropiedad",
+  verificarToken,
+  obtenerCitasPorPropiedad
+);
+citasRouter.get(
+  "/getCitasByEstatus/:estatus",
+  verificarToken,
+  obtenerCitasPorEstatus
+);
+citasRouter.get(
+  "/getCitasByFechas",
+  verificarToken,
+  obtenerCitasPorRangoFechas
+);
+citasRouter.post("/postCita", verificarToken, crearCita);
+citasRouter.put("/putCita", verificarToken, actualizarCita);
+citasRouter.get("/getHorariosDisponibles", obtenerHorariosDisponibles);
+citasRouter.delete("/deleteCita/:idCita", verificarToken, eliminarCita);
+citasRouter.patch(
+  "/patchCita/:idCita/estatus",
+  verificarToken,
+  actualizarEstatusCita
+);
 
 export default citasRouter;
